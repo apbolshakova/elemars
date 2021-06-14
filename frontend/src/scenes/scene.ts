05f98e88d2ebbac2d71cbbb6481a1386d391e19f
julody <julody@mail.ru>
2021-06-14 21:22:16 +0300

    chore(front): fix eslint config

--- frontend/src/scenes/scene.ts
+++ frontend/src/scenes/scene.ts
@@ -1,26 +1,26 @@
-// type ObjectTransform = Phaser.GameObjects.GameObject & Phaser.GameObjects.Components.Transform;
+// type ObjectTransform = Phaser.GameEntities.GameEntity & Phaser.GameEntities.Components.Transform;
 
 export default class Scene extends Phaser.Scene {
-  constructor() {
-    super({
-      key: "Scene",
-    });
-  }
+    constructor() {
+        super({
+            key: 'Scene',
+        });
+    }
 
-  preload(): void {
-    this.load.image("logo", "src/assets/logo.png");
-  }
+    preload(): void {
+        this.load.image('logo', 'src/assets/logo.png');
+    }
 
-  create(): void {
-    const logo = this.add.image(400, 150, "logo");
+    create(): void {
+        const logo = this.add.image(400, 150, 'logo');
 
-    this.tweens.add({
-      targets: logo,
-      y: 450,
-      duration: 2000,
-      ease: "Power2",
-      yoyo: true,
-      loop: -1,
-    });
-  }
+        this.tweens.add({
+            targets: logo,
+            y: 450,
+            duration: 2000,
+            ease: 'Power2',
+            yoyo: true,
+            loop: -1,
+        });
+    }
 }
